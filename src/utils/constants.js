34cf4ae05f7169dae2b39fcb6e45fb40ff4cf035
Himanshu Dhiman <himanshudhiman@Himanshus-MacBook-Pro.local>
2024-11-25 16:38:15 +0530

    hidden key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
-const API_KEY = "AIzaSyA756cFvUzwgFR6aaduni-wbral-P8VPKY";
+// const API_KEY = "AIzaSyA756cFvUzwgFR6aaduni-wbral-P8VPKY";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
-  API_KEY;
+  process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
